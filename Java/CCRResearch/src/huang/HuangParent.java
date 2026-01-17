179cc91536f3146c0fc1aac936681ace150d6680
Al-ttaeeA <ahmedalttaee0306@gmail.com>
2026-01-17 18:39:29 -0500

    Basic setup for Huang Parent rule finding class

---
+++ Java/CCRResearch/src/huang/HuangParent.java
@@ -0,0 +1,16 @@
+package huang;
+
+public class HuangParent {
+	/************************************************
+	 * Enter n value here to use the program 		*
+	 ************************************************/
+	public static int n = 6;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static String parent(String childRep) {
+		
+	}
+}
