0cb51f247a326a102fbf19038acf28847a1a4523
Al-ttaeeA <ahmedalttaee0306@gmail.com>
2026-01-17 19:07:04 -0500

    Finished parent rule, though it is wrong, further investigations required

--- Java/CCRResearch/src/huang/HuangParent.java
+++ Java/CCRResearch/src/huang/HuangParent.java
@@ -7,7 +7,7 @@ public class HuangParent {
 	public static int n = 6;
 	
 	public static void main(String[] args) {
-		System.out.println(difference("001100"));
+		System.out.println(parent("001010110101"));
 	}
 	
 	/*******************************************
@@ -21,6 +21,8 @@ public class HuangParent {
 		for(int i = 0; i < 2*n; i++) {
 			String curSubstring = childRepExt.substring(i, i+n);
 			
+			System.out.println(i + "   " + curSubstring);
+			
 			//Last digit has to be 1
 			if(curSubstring.charAt(n-1) != '1') {
 				continue;
@@ -28,8 +30,17 @@ public class HuangParent {
 			
 			String curReversePCR = reversePCR(curSubstring);
 			
+			int diffCurSubstring = difference(curSubstring);
+			int diffReversePCR = difference(curReversePCR);
 			
+			System.out.println(curReversePCR + "   " + diffReversePCR + "   " + diffCurSubstring);
+			
+			if(diffReversePCR < diffCurSubstring) {
+				return getRep(curReversePCR);
+			}
 		}
+		
+		return null;
 	}
 	
 	/***************************************
@@ -40,16 +51,11 @@ public class HuangParent {
 	public static int difference(String str) {
 		str = str + complement(str.charAt(0));
 		
-		char dupChar = (char) (complement(str.charAt(0)) + '0');
-		
 		int difference = 0;
 		
 		for(int i = 0; i < n; i++) {
 			if(str.charAt(i) != str.charAt(i+1)) {
 				difference++;
-			}
-			else if(str.charAt(i) == dupChar) {
-				difference += 2;
 			}
 		}
 		
@@ -74,5 +80,67 @@ public class HuangParent {
 	 ******************************************/
 	public static String reversePCR(String str) {
 		return str.charAt(n-1) + str.substring(0, n-1);
+	}
+	
+	/**************************************
+	 * Method to find the representative k*n length string of an initial string
+	 * @param str - initial string
+	 * @return returns the representative
+	 **************************************/
+	public static String getRep(String str) {
+		String extendedStr = extendString(str);
+		String rep = extendedStr; //start with declaring representative as the extended string
+		
+		for(int i = 0; i < extendedStr.length(); i++) {
+			extendedStr = nextCycle(extendedStr); //get next cycle
+			
+			if(extendedStr.compareTo(rep) < 0) { //if the new cycle is lexicographically less than the rep, then switch the rep
+				rep = extendedStr;
+			}
+		}
+		
+		return rep; //Return the found rep
+	}
+	
+	/********************************************
+	 * Method to extend a n-length string to its CCR function based long string
+	 * @param str - initial string
+	 * @return returns the extended form of the string
+	 ********************************************/
+	public static String extendString(String str) {
+		String extendedStr = str; //start with the string
+		
+		if(extendedStr.length() == 2*n) return extendedStr;
+		
+		//extend the string
+		for(int i = 0; i < (2-1) * n; i++) {
+			extendedStr += nextChar(extendedStr.substring(i));
+		}
+		
+		return extendedStr;
+	}
+	
+	/*****************************************
+	 * Method to rotate the string to the next cycle
+	 * @param str - initial String
+	 * @return return the next cycle
+	 *****************************************/
+	public static String nextCycle(String str) {
+		String nextStr = str.substring(1); //copy the last n-1 digits as the first n-1 digit
+		nextStr += str.charAt(0); //copy the first
+		
+		return nextStr;
+	}
+	
+	/****************************************
+	 * Method to find the next char after a string using the CCR feedback function
+	 * @param str - initial string
+	 * @return returns the next char
+	 ****************************************/
+	public static String nextChar(String str) {
+		String nextChar = "";
+		nextChar += (str.charAt(0) - '0' + 1) % 2; //add a_1 plus 1 modulus k
+		
+		return nextChar; 
 	}
 }
