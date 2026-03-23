c573e5e78c32451b69e7fde26480656ea9de4a4b
Al-ttaeeA <ahmedalttaee0306@gmail.com>
2026-03-23 18:50:01 -0400

    Progress on Cycle tree construction for MSR

--- Java/MSRResearch/src/cycletree/CycleTreeConstruction.java
+++ Java/MSRResearch/src/cycletree/CycleTreeConstruction.java
@@ -2,19 +2,19 @@ package cycletree;
 
 import java.util.*;
 
-class node {
+class Node {
 	public String rep; //lex least rotation of of the MSR cycle, length is n
-	public node parent;
+	public Node parent;
 	public int visited = 0;
-	public ArrayList<node> children;
+	public ArrayList<Node> children;
 	
-	node(String rep, node parent) {
+	Node(String rep, Node parent) {
 		this.rep = rep;
 		this.parent = parent;
-		this.children = new ArrayList<node>();
+		this.children = new ArrayList<Node>();
 	}
 	
-	public void addChild(node child) {
+	public void addChild(Node child) {
 		this.children.add(child);
 	}
 	
@@ -32,16 +32,38 @@ class node {
 public class CycleTreeConstruction {
 	public static int n = 4;
 	
-	public static String UC = "";
+	public static String UC = "123124132134214324314234";
+	
+	public static HashMap<String, Node> map = new HashMap<String, Node>();
 	
 	public static void main(String[] args) {
-		System.out.println(lexLeastMSR("123"));
+		System.out.println(findRep("341"));
 		//function();
 	}
 	
 	public static void function() {
+		String start = "";
 		
+		for(int i = 1; i < n; i++) {
+			start += i;
+		}
+		
+		String startExtended = start + missingSymbol(start);
+		
+		Node startNode = new Node(startExtended, null);
+		
+		map.put(startExtended, startNode);
+		
+		String UCextended = UC + UC.substring(0, n-1);
+		
+		for(int i = 0; i < UC.length(); i++) {
+			String window = UCextended.substring(i, n-1 + i);
+			
+			
+		}
 	}
+	
+	
 	
 	
 	public static String lexLeastMSR(String str) {
@@ -57,6 +79,14 @@ public class CycleTreeConstruction {
 		String next = str.substring(1) + missingSymbol(str);
 		
 		return next;
+	}
+	
+	public static String findRep(String str) {
+		String lex = lexLeastMSR(str);
+		
+		lex += missingSymbol(lex);
+		
+		return lex;
 	}
 	
 	public static char missingSymbol(String str) {
